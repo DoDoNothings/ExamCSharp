804045fbabd805a21e19466f7ba632ae98ac71bc
Vavi_sq66 <Vavi_sq66@student.itstep.org>
2022-01-28 13:19:26 +0200

    new

--- DataBase/Migrations/Pivot_Colors_Modification.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Pivot_Colors_Modification]
-(
-	[ColorID] UNIQUEIDENTIFIER NOT NULL , 
-    [ModificationID] UNIQUEIDENTIFIER NOT NULL
-)
--- DataBase/Migrations/Pivot_Modification_Model.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Pivot_Modification_Model]
-(
-	[ModificationID] UNIQUEIDENTIFIER NOT NULL, 
-    [ModelID] UNIQUEIDENTIFIER NOT NULL 
-)
